8ca949538795d88174524423a000836550582fdd
jack james <scunnyrule45@hotmail.co.uk>
2017-10-17 21:37:29 +0100

    Quest Entity Created

---
+++ EpicQuestEntities/EpicQuestEntities/Quest.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+
+namespace EpicQuestEntities
+{
+    public class Quest
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+    public class QuestValidator : AbstractValidator<Quest>
+    {
+        public QuestValidator()
+        {
+            RuleFor(quest => quest.Id).NotEmpty().WithMessage("Id is required");
+            RuleFor(quest => quest.Title).NotEmpty().WithMessage("Title is required");
+        }
+    }
+
+}
