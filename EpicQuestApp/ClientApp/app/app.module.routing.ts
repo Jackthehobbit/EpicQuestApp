17b0eb41a70104f92e151fb6ae616202bf48afa4
jack james <scunnyrule45@hotmail.co.uk>
2017-10-19 18:23:25 +0100

    Setup Client Side with CSS Js

---
+++ EpicQuestApp/ClientApp/app/app.module.routing.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Import any compnents required for the routes
+import { HomeComponent } from './components/home/home.component';
+
+//Define the routes used by the application
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: '**', redirectTo: 'home' }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
---
+++ EpicQuestApp/ClientApp/app/components/Quests/quest.ts
@@ -0,0 +1,5 @@
+﻿export class Quest {
+    id: number;
+    name: string;
+    description: string;
+}
