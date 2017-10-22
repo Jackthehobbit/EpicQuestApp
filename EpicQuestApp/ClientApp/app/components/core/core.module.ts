9d6cfe2ec219909db563ea81e33a7ad3a08c9380
jack james <scunnyrule45@hotmail.co.uk>
2017-10-22 16:56:50 +0100

    Added Message component and core service for updating it.

--- EpicQuestApp/ClientApp/app/components/core/core.module.ts
+++ EpicQuestApp/ClientApp/app/components/core/core.module.ts
@@ -1,17 +1,25 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalDialogComponent } from './modal-dialog/modal-dialog.component'
+import { MessageComponent } from'./message/message.component'
+
+import { CoreService } from './core.service'
 
 
 @NgModule({
     declarations: [
-        ModalDialogComponent
+        ModalDialogComponent,
+        MessageComponent
     ],
     imports: [
         CommonModule
     ],
     exports: [
-        ModalDialogComponent
+        ModalDialogComponent,
+        MessageComponent
+    ],
+    providers: [
+        CoreService
     ]
 })
 
